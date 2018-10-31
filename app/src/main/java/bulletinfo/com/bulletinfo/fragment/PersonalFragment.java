da37a21b9a97f4d3907d6503474f2307722b6130
13218 <1321840134@qq.com>
2018-10-31 14:38:36 +0800

    添加连接socket服务器

--- app/src/main/java/bulletinfo/com/bulletinfo/fragment/PersonalFragment.java
+++ app/src/main/java/bulletinfo/com/bulletinfo/fragment/PersonalFragment.java
@@ -1,7 +1,6 @@
 package bulletinfo.com.bulletinfo.fragment;
 
 
-import android.content.Intent;
 import android.os.Bundle;
 
 import android.support.annotation.Nullable;
@@ -9,13 +8,11 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.Button;
 
-import bulletinfo.com.bulletinfo.activity.LoginActivity;
 import bulletinfo.com.bulletinfo.R;
 
 public class PersonalFragment extends Fragment implements View.OnClickListener {
-    private Button personal_login;
+
 
     @Nullable
     @Override
@@ -26,18 +23,16 @@ public class PersonalFragment extends Fragment implements View.OnClickListener {
     @Override
     public void onActivityCreated(@Nullable Bundle savedInstanceState) {
         super.onActivityCreated(savedInstanceState);
-        personal_login = getActivity().findViewById(R.id.personal_login);
-        personal_login.setOnClickListener(this);
+        initView();
+    }
 
+    private void initView() {
     }
 
     @Override
     public void onClick(View v) {
         switch (v.getId()){
-            case R.id.personal_login:
-                Intent intent = new Intent(getActivity(), LoginActivity.class);
-                startActivity(intent);
-                break;
+
         }
     }
 }
